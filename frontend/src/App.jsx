4af074bd7607941d0f1c7e6bfa9da52d9683af6b
rajkharvar <rajkharvar1808@gmail.com>
2021-12-06 19:08:16 +0530

    Added button to connect with metamask

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,49 +6,78 @@ import Payment from "./components/Payment";
 import WalletInfo from "./components/WalletInfo";
 import Warn from "./components/Warn";
 import Web3Modal from "web3modal";
+import Connect from "./components/Connect";
 
 function App() {
   const [isMetamaskConnected, setIsMetamaskConnected] = useState();
+  const [isMetamaskInstalled, setIsMetamaskInstalled] = useState();
   const [chainId, setChainId] = useState(null);
   const [networkName, setNetworkName] = useState(null);
   const [address, setAddress] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  const fetchNetworkDetails = async (ethereum) => {
+  const fetchNetworkDetails = async () => {
+    try {
+      const { ethereum } = window;
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const { chainId, name } = await provider.getNetwork();
+      const signer = await provider.getSigner();
+      const address = await signer.getAddress();
+      setChainId(chainId);
+      setNetworkName(name);
+      setAddress(address);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkAccountConnected = async () => {
     const provider = new ethers.providers.Web3Provider(ethereum);
-    const { chainId, name } = await provider.getNetwork();
-    const signer = await provider.getSigner();
-    const address = await signer.getAddress();
-    setChainId(chainId);
-    setNetworkName(name);
-    setAddress(address);
-    console.log("Name:", name);
-    setIsLoading(false);
+    const accounts = await provider.listAccounts();
+    if (!accounts.length) {
+      setIsMetamaskConnected(false);
+      return;
+    }
+    setIsMetamaskConnected(true);
   };
 
   const connect = async () => {
-    const web3modal = new Web3Modal();
-    await web3modal.connect();
+    try {
+      const web3modal = new Web3Modal();
+      const result = await web3modal.connect();
+      if (result) {
+        setIsMetamaskConnected(true);
+        fetchNetworkDetails();
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
     const { ethereum } = window;
     if (ethereum) {
-      connect();
-      setIsMetamaskConnected(true);
+      setIsMetamaskInstalled(true);
+      checkAccountConnected();
       fetchNetworkDetails(ethereum);
+    } else {
+      setIsMetamaskInstalled(false);
     }
   }, []);
   return (
     <div className="mx-48 px-48 pt-28">
       <Header address={address} />
-      {!isLoading && address ? (
+      {isMetamaskInstalled ? (
+        !isMetamaskConnected && <Connect connect={connect} />
+      ) : (
+        <Warn />
+      )}
+      {!isLoading && address && (
         <>
           <WalletInfo address={address} />
           <Payment address={address} />
         </>
-      ) : (
-        <Warn />
       )}
     </div>
   );
