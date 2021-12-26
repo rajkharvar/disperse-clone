a9acf704af714fb2f7d262cbad03340b930e22c7
rajkharvar <rajkharvar1808@gmail.com>
2021-12-26 18:08:39 +0530

    Disabled button when balance is less than total

--- frontend/src/components/Confirm.jsx
+++ frontend/src/components/Confirm.jsx
@@ -1,4 +1,5 @@
 import { ethers } from "ethers";
+import { useState, useEffect } from "react";
 
 const Confirm = ({
   recipientsData,
@@ -10,6 +11,14 @@ const Confirm = ({
   disperse,
   isDisperseSuccessful,
 }) => {
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    if (total && tokenBalance) {
+      setIsDisabled(!ethers.utils.parseUnits(tokenBalance).gt(total));
+    }
+  }, [total, tokenBalance]);
+
   return (
     <div className="pt-16">
       <h3 className="text-2xl font-light italic">confirm</h3>
@@ -46,7 +55,11 @@ const Confirm = ({
           </div>
         </li>
         <li>
-          <div className="flex justify-between mt-2">
+          <div
+            className={`flex justify-between mt-2 ${
+              isDisabled && "text-red-700"
+            }`}
+          >
             <p className="italic">remaining</p>
             <p className="italic">{remaining}</p>
           </div>
@@ -57,20 +70,27 @@ const Confirm = ({
         <div className="mt-4 flex items-center">
           <button
             onClick={approve}
-            className="px-2 py-3 italic"
+            className={`px-2 py-3 italic disabled:opacity-50 cursor-pointer ${
+              isDisabled && `disabled:cursor-default`
+            }`}
             style={{
               background: "aquamarine",
               boxShadow: "6px 6px crimson",
             }}
+            disabled={isDisabled}
           >
             Approve
           </button>
+          {isDisabled && <p className="ml-4 italic">total exceeds balance</p>}
           {isApproved && <p className="ml-4">Token approved</p>}
         </div>
         <div className="mt-6 flex items-center">
           <button
             onClick={disperse}
-            className="px-2 py-3 italic"
+            disabled={isDisabled}
+            className={`px-2 py-3 italic disabled:opacity-50 cursor-pointer ${
+              isDisabled && `disabled:cursor-default`
+            }`}
             style={{
               background: "aquamarine",
               boxShadow: "6px 6px crimson",
@@ -78,6 +98,7 @@ const Confirm = ({
           >
             disperse token
           </button>
+          {isDisabled && <p className="ml-4 italic">total exceeds balance</p>}
           {isDisperseSuccessful && (
             <p className="ml-4">Disperse token successful 🎉</p>
           )}
--- frontend/src/components/ConfirmEther.jsx
+++ frontend/src/components/ConfirmEther.jsx
@@ -1,4 +1,5 @@
 import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 
 const ConfirmEther = ({
   recipientsData,
@@ -8,6 +9,14 @@ const ConfirmEther = ({
   disperse,
   isDisperseSuccessful,
 }) => {
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    if (total && tokenBalance) {
+      setIsDisabled(!ethers.utils.parseUnits(tokenBalance).gt(total));
+    }
+  }, [total, tokenBalance]);
+
   return (
     <div className="pt-16">
       <h3 className="text-2xl font-light italic">confirm</h3>
@@ -44,29 +53,39 @@ const ConfirmEther = ({
           </div>
         </li>
         <li>
-          <div className="flex justify-between mt-2">
+          <div
+            className={`flex justify-between mt-2 ${
+              isDisabled && "text-red-700"
+            }`}
+          >
             <p className="italic">remaining</p>
             <p className="italic">{remaining}</p>
           </div>
         </li>
       </ul>
-      <div className="mt-8">
-        <div className="mt-6 flex items-center">
-          <button
-            onClick={disperse}
-            className="px-2 py-3 italic"
-            style={{
-              background: "aquamarine",
-              boxShadow: "6px 6px crimson",
-            }}
-          >
-            disperse ether
-          </button>
-          {isDisperseSuccessful && (
-            <p className="ml-4">Disperse token successful 🎉</p>
-          )}
+      {total && tokenBalance && (
+        <div className="mt-8">
+          <div className="mt-6 flex items-center ">
+            <button
+              onClick={disperse}
+              disabled={isDisabled}
+              className={`px-2 py-3 italic disabled:opacity-50 cursor-pointer ${
+                isDisabled && `disabled:cursor-default`
+              }`}
+              style={{
+                background: "aquamarine",
+                boxShadow: "6px 6px crimson",
+              }}
+            >
+              disperse ether
+            </button>
+            {isDisabled && <p className="ml-4 italic">total exceeds balance</p>}
+            {isDisperseSuccessful && (
+              <p className="ml-4">Disperse token successful 🎉</p>
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
