f1edd493ca1a3cd798cebc28b937ca70ad101a31
rajkharvar <rajkharvar1808@gmail.com>
2022-03-28 22:46:14 +0530

    Support skale network on frontend

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,9 +1,10 @@
-const chains = {
-  3: "ropsten",
-  4: "rinkeby",
-  5: "goerli",
-  42: "kovan",
-  80001: "maticmum",
+export const chains = {
+  ropsten: 3,
+  rinkeby: 4,
+  goerli: 5,
+  kovan: 42,
+  maticmum: 80001,
+  skale: 416452918254875,
 };
 
 export const disperseAddresses = {
@@ -22,6 +23,9 @@ export const disperseAddresses = {
   80001:
     import.meta.VITE_DISPERSE_MATIC_MUMBAI ||
     "0xaf6f4A737188571d128477DEe81d54Bc63De6d80",
+  416452918254875:
+    import.meta.VITE_DISPERSE_SKALE ||
+    "0x73a41d6b7889fBF94d059C943bc58dd18c600bfa",
 };
 
 export const scans = {
@@ -30,7 +34,8 @@ export const scans = {
   5: "https://goerli.etherscan.io/",
   42: "https://kovan.etherscan.io/",
   80001: "https://mumbai.polygonscan.com/",
+  416452918254875: "http://faint-acubens.explorer.dappnet.skalenodes.com/",
 };
 
 export const warnMessage =
-  "*Supports ropsten, rinkeby, goerli, kovan and mumbai(matic)*";
+  "*Supports ropsten, rinkeby, goerli, kovan, skale and mumbai(matic)*";
